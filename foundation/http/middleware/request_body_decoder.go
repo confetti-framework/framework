392b9908e71d6cc1678b705caa84d1a6b1246f96
reindert-vetter <reindertvetter@gmail.com>
2023-07-05 22:25:12 +0200

    Fix header not found

--- foundation/http/middleware/request_body_decoder.go
+++ foundation/http/middleware/request_body_decoder.go
@@ -8,10 +8,9 @@ import (
 
 type RequestBodyDecoder struct{}
 
-// This ensures the request can be decoded from JSON and Multipart Form.
+// Handle ensures that the request can be decoded from JSON and Multipart Form.
 // Feel free to add your own decoders to support other formats as well.
 func (r RequestBodyDecoder) Handle(request inter.Request, next inter.Next) inter.Response {
-
 	switch {
 	case http_helper.HasJson(request):
 		request.App().Bind(inter.RequestBodyDecoder, encoder.RequestWithJsonToValue)
